dfe03c06496e8d00998b4c12fc01ac9c68baac0b
RZ PEDIA <72466978+rulsz@users.noreply.github.com>
2024-09-04 22:19:20 +0700

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -23,18 +23,19 @@ const games = {
         appToken: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
         promoId: 'ef319a80-949a-492e-8ee0-424fb5fc20a6'
     },
-    'Cafe': {
-        appToken: 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11',
-        promoId: 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11'
-    },
-    'Gang': {
-        appToken: 'b6de60a0-e030-48bb-a551-548372493523',
-        promoId: 'c7821fa7-6632-482c-9635-2bd5798585f9'
-    },
     'Zoopolis': {
         appToken: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
         promoId: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b'
+    },
+    'Fluff': {
+        appToken: '112887b0-a8af-4eb2-ac63-d82df78283d9',
+        promoId: '112887b0-a8af-4eb2-ac63-d82df78283d9'
+    },
+    'Tile': {
+        appToken: 'e68b39d2-4880-4a31-b3aa-0393e7df10c7',
+        promoId: 'e68b39d2-4880-4a31-b3aa-0393e7df10c7'
     }
+
 }
 const urls = {
     login: 'https://api.gamepromo.io/promo/login-client',
